26c4a733642db26357e126f222fc4ca911645e5b
hachiri <112574700+hachiri9511@users.noreply.github.com>
2022-12-12 11:56:44 -0500

    Add files via upload

---
+++ js.java/java.js
@@ -0,0 +1,4 @@
+document.getElementById("ingresar").addEventListener("click",function(){
+    let email = document.getElementById("email").value;
+    let pass  = document.getElementById("pass").value;
+})
